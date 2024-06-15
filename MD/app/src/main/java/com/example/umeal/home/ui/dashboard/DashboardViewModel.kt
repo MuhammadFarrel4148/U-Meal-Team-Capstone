7dc447fd06492cf494531ab5adfb6bd7eb46efae
Syaroful <syaroful10403@gmail.com>
2024-06-15 11:44:27 +0700

    feat: add calorie count feature

--- MD/app/src/main/java/com/example/umeal/home/ui/dashboard/DashboardViewModel.kt
+++ MD/app/src/main/java/com/example/umeal/home/ui/dashboard/DashboardViewModel.kt
@@ -3,11 +3,32 @@ package com.example.umeal.home.ui.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.jvm.Throws
 
 class DashboardViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun countCalorie(
+        bb: Double,
+        tb: Double,
+        age: Double,
+        tr: Int,
+        facActivity: String,
+        facStress: String
+    ): Double {
+        val bee = 655 + (9.6 * bb) + (1.85 * tb) - (4.68 * age)
+        val tee = bee * getFactorValue(facActivity) * getFactorValue(facStress)
+        return when (tr) {
+            1 -> tee
+            2 -> tee + 300
+            3 -> tee + 600
+            else -> {
+                throw IllegalArgumentException("Invalid trimester")
+            }
+        }
     }
-    val text: LiveData<String> = _text
+
+    private fun getFactorValue(string: String): Double {
+        val parts = string.split(" - ")
+        return parts[1].toDoubleOrNull() ?: 0.0
+    }
 }
